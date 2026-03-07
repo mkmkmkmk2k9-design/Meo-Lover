567d9db1983eb5a9947166207630fd035bcde251
mkmkmkmk2k9-design <mkmkmkmk2k9@gmail.com>
2026-03-07 09:11:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -216,7 +216,7 @@ async function init(text) {
 
     let textNodes = [];
 
-    let step = isMobileDevice() ? 0.7 : 1.2;
+    let step = isMobileDevice() ? 0.6 : 1.2;
 
     for (let y = 0; y < offCanvas.height; y += step * scale) {
 
@@ -466,3 +466,4 @@ document.fonts.ready.then(async () => {
     animate();
 
 });
+
